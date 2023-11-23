03c12fe34c4bf14a2c324974d9506be0d76fb20a
grace7040 <81251069+grace7040@users.noreply.github.com>
2023-11-23 15:42:31 +0900

    플레이타임 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,7 +16,7 @@ public class GameManager : MonoBehaviourPunCallbacks
     PlayerManager playerManager;
 
     private float time;
-    private float gameTime = 90f;
+    private float gameTime = 120f;
 
     [HideInInspector]
     public bool isEnd;
